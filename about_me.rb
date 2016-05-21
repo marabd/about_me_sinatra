c39f112910d46098f1dcb142f0a07dd9f7886280
Mara Behles Dean <maraebehles@gmail.com>
2016-05-21 15:17:23 -0600

    first commit

---
+++ about_me.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'pry'
+
+get '/about_me' do
+	erb :intro
+end
+
+get'/contact' do
+	erb :contact
+end
+
+get '/resume' do
+	erb :resume
+end
+
+post '/contact' do
+	"Thank you!"
+end
+
+not_found do
+	status 404
+	'not found'
+end
